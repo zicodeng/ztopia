f935ae8b54724544f3f52e74259fe27bf7a1309e
Zico Deng <zicodeng@gmail.com>
2017-06-28 17:51:43 -0700

    Add 404 page

--- themes/ztopia/404.php
+++ themes/ztopia/404.php
@@ -1,1 +1,15 @@
-<?php echo "404" ?>
+<?php
+// Get global header
+get_header();
+?>
+
+<main class="page-404">
+	<h1>404</h1>
+	<span class="shadow"></span>
+	<a href="http://www.zicodeng.me">BACK TO HOME</a>
+</main>
+
+<?php
+// Get global footer
+get_footer();
+?>
--- themes/ztopia/page-thank-you.php
+++ themes/ztopia/page-thank-you.php
@@ -6,6 +6,7 @@ get_header();
 <main class="thank-you">
 	<h1>Thank You!</h1>
 	<p>Thanks for reaching me out. I will contact you back shortly if your message worth my attention.</p>
+	<a href="http://www.zicodeng.me">BACK TO HOME</a>
 </main>
 
 <?php
